def21835acc81aa44c99231206acf22820359bfb
galang <galang050106@gmail.com>
2026-03-12 12:11:42 +0700

    add file TugasBFS2

---
+++ TugasDFS2.py
@@ -0,0 +1,37 @@
+graph = {
+    "Rumah": ["Kampus", "Masjid"],
+    "Kampus": ["Terminal"],
+    "Masjid": ["Mall"],
+    "Mall": [],
+    "Terminal": ["Rumah Sakit"],
+    "Rumah Sakit": []
+}
+
+def dfs(node, goal, path, visited):
+    path.append(node)
+    visited.add(node)
+
+    if node == goal:
+        return path
+
+    for neighbor in graph[node]:
+        if neighbor not in visited:
+            result = dfs(neighbor, goal, path.copy(), visited)
+            if result:
+                return result
+
+    return None
+
+start = "Rumah"
+goal = "Rumah Sakit"
+
+path = dfs(start, goal, [], set())
+
+print("Start :", start)
+print("Goal :", goal)
+
+print("\nJalur yang ditemukan:")
+if path:
+    print(" -> ".join(path))
+else:
+    print("Tidak ditemukan jalur")
